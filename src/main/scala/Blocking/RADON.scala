7305643d286f5fc36ce72b4984f5b2ea8f695964
GeorgeM <giwrgosmandi@gmail.com>
2020-02-03 17:11:19 +0200

    blocking and swapping

--- src/main/scala/Blocking/RADON.scala
+++ src/main/scala/Blocking/RADON.scala
@@ -2,8 +2,11 @@ package Blocking
 
 import DataStructures.GeoProfile
 import org.apache.spark.rdd.RDD
+import utils.Constant
 
-object RADON {
+class RADON	(var sourceRDD: RDD[GeoProfile], var targetRDD: RDD[GeoProfile], var relation: String) {
+
+	var swapped = false
 
 	def getETH(gpRDD: RDD[GeoProfile]): Double ={
 		getETH(gpRDD, gpRDD.count())
@@ -18,4 +21,54 @@ object RADON {
 		val eth = count * ( (denom * coords_sum._1) * (denom * coords_sum._2) )
 		eth
 	}
+
+	// TODO learn about the theta value of RADON
+	def swappingStrategy(): Unit= {
+		val sourceETH = getETH(sourceRDD)
+		val targetETH = getETH(targetRDD)
+
+		if (targetETH < sourceETH){
+			swapped = true
+			val temp = sourceRDD
+			sourceRDD = targetRDD
+			targetRDD = temp
+
+			relation =
+				relation match {
+				case Constant.WITHIN => Constant.CONTAINS
+				case Constant.CONTAINS => Constant.WITHIN
+				case Constant.COVERS => Constant.COVEREDBY
+				case Constant.COVEREDBY => Constant.COVERS;
+				case _ => relation
+			}
+		}
+	}
+
+
+	def sparseSpaceTiling(): Unit = {
+		swappingStrategy()
+
+		val blocks = sourceRDD
+			.map {
+				gp =>
+					val gpID = gp.id
+					var blockIDs: Array[(Int, Int)] = Array()
+					val maxX = math.ceil(gp.maxX).toInt
+					val minX = math.ceil(gp.minX).toInt
+					val maxY = math.ceil(gp.maxY).toInt
+					val minY = math.ceil(gp.minY).toInt
+
+
+					// TODO split if it crossed meridian
+					// MBBIndex westernPart = new MBBIndex(minLatIndex, (int) Math.floor(-180d * thetaX), maxLatIndex,	minLongIndex, g, p + "<}W", p);
+					//MBBIndex easternPart = new MBBIndex(minLatIndex, maxLongIndex, maxLatIndex, (int) Math.ceil(180 * thetaX), g, p + "<}E", p);
+
+
+					(minX to maxX).map(x => (minY to maxY).map(y => blockIDs :+= (x, y)))
+					(blockIDs, gpID)
+			}
+    		.flatMap(p => p._1.map(blockID => (blockID, Array(p._2))))
+			.reduceByKey(_++_)
+		print()
+		}
 }
