a29ac3eb0bf2815d3450bd1c9e2abd07b6a84a43
GeorgeM <giwrgosmandi@gmail.com>
2020-02-03 17:12:07 +0200

    constants

--- src/main/scala/utils/Constant.scala
+++ src/main/scala/utils/Constant.scala
@@ -3,6 +3,16 @@ package utils
 object Constant {
 
 	val DT_SOURCE = "source"
-
 	val DT_TARGET = "target"
+
+	val EQUALS = "equals"
+	val DISJOINT = "disjoint"
+	val INTERSECTS = "intersects"
+	val TOUCHES = "touches"
+	val CROSSES = "crosses"
+	val WITHIN = "within"
+	val CONTAINS = "contains"
+	val OVERLAPS = "overlaps"
+	val COVERS = "covers"
+	val COVEREDBY = "coveredby"
 }
