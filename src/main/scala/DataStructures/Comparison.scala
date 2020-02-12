99b46be4d5b23d7d11156de5e64ea77721e671ec
GiorgosMandi <giwrgosmandi@gmail.com>
2020-02-12 17:43:36 +0200

    comparisons

---
+++ src/main/scala/DataStructures/Comparison.scala
@@ -0,0 +1,11 @@
+package DataStructures
+
+import utils.BlockUtils
+
+case class Comparison(id: Int, entity1_id: Int, entity2_id: Int)
+
+object Comparison {
+	def apply(id1: Int, id2: Int): Comparison ={
+		Comparison(BlockUtils.bijectivePairing(id1, id2), id1, id2)
+	}
+}
