c0d92065de0b56c69bd61e3dc226558ed3818767
George <giwrgosmandi@gmail.com>
2020-02-18 14:25:33 +0200

    silk blocking

--- src/main/scala/Blocking/StaticBlocking.scala
+++ src/main/scala/Blocking/StaticBlocking.scala
@@ -1,6 +1,8 @@
 package Blocking
 
 import DataStructures.{Block, SpatialEntity}
+import org.apache.spark.SparkContext
+import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
 import utils.Constants
 
@@ -17,23 +19,22 @@ case class StaticBlocking (var source: RDD[SpatialEntity], var target: RDD[Spati
 				if (distance != 0.0)
 					envelope.expandBy((distance / Constants.EARTH_CIRCUMFERENCE_EQUATORIAL) * Constants.LONG_RANGE, (distance / Constants.EARTH_CIRCUMFERENCE_MERIDIONAL) * Constants.LAT_RANGE)
 
-				val blockCountLat = (Constants.LAT_RANGE*blockingParameter).toInt
-				val blockCountLong = (Constants.LONG_RANGE*blockingParameter).toInt
-
 				val minLatBlock = (envelope.getMinY*blockingParameter).toInt
 				val maxLatBlock = (envelope.getMaxY*blockingParameter).toInt
 				val minLongBlock = (envelope.getMinX*blockingParameter).toInt
 				val maxLongBlock = (envelope.getMaxX*blockingParameter).toInt
 
-				val latBlocks = (for (i <- minLatBlock to maxLatBlock) yield i).toSet
-				val longBlocks = (for(i <- minLongBlock to maxLongBlock) yield i).toSet
+				//TODO: crosses meridian case
+				val blockIDs = for(x <- minLongBlock to maxLongBlock; y <- minLatBlock to maxLatBlock) yield (x, y)
+				(blockIDs, se.id)
 		}
-    		.collect()
-		null
-
-		//Index.oneDim(latBlocks, blockCountLat) conjunction Index.oneDim(longBlocks, blockCountLong)
-
-
+		if (acceptedBlocks.nonEmpty) {
+			// filters out blocks that don't contain entities of the source
+			val acceptedBlocksBD = SparkContext.getOrCreate().broadcast(acceptedBlocks)
+			broadcastMap += ("acceptedBlocks" -> acceptedBlocksBD.asInstanceOf[Broadcast[Any]])
+			blocks.flatMap(p => p._1.filter(acceptedBlocksBD.value.contains).map(blockID => (blockID, Array(p._2)))).reduceByKey(_ ++ _)
+		}
+		else blocks.flatMap(p => p._1.map(blockID => (blockID, Array(p._2)))).reduceByKey(_++_)
 	}
 
 
