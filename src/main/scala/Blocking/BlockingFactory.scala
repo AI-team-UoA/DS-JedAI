63d542b6e3851ce026924165c0a8822fe3a58e1d
GiorgosMandi <giwrgosmandi@gmail.com>
2020-02-21 13:22:50 +0200

    comments

---
+++ src/main/scala/Blocking/BlockingFactory.scala
@@ -0,0 +1,25 @@
+package Blocking
+
+import DataStructures.SpatialEntity
+import org.apache.spark.rdd.RDD
+import utils.{Configuration, Constants}
+
+/**
+ * @author George Mandilaras < gmandi@di.uoa.gr > (National and Kapodistrian University of Athens)
+ */
+object BlockingFactory {
+
+	def getBlocking(conf: Configuration, source: RDD[SpatialEntity], target: RDD[SpatialEntity]): Blocking = {
+		val algorithm = conf.configurations.getOrElse(Constants.CONF_BLOCK_ALG, Constants.RADON)
+		algorithm match {
+			case Constants.STATIC_BLOCKING =>
+				val blockingFactor: Int =conf.configurations.getOrElse(Constants.CONF_SPATIAL_BLOCKING_FACTOR, "10").toInt
+				val distance: Double = conf.configurations.getOrElse(Constants.CONF_STATIC_BLOCKING_DISTANCE, "0.0").toDouble
+				StaticBlocking(source, target, blockingFactor, distance)
+
+			case Constants.RADON| _ =>
+				val theta_msr = conf.configurations.getOrElse(Constants.CONF_THETA_MEASURE, Constants.NO_USE)
+				RADON(source, target, theta_msr)
+		}
+	}
+}
