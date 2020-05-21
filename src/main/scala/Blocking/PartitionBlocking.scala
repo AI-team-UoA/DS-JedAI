decf292635fc97b2d531f79d225918c001aaf94c
George <giwrgosmandi@gmail.com>
2020-05-21 12:00:59 +0300

    spatial partitioning

---
+++ src/main/scala/Blocking/PartitionBlocking.scala
@@ -0,0 +1,82 @@
+package Blocking
+
+import DataStructures.{Block, MBB, SpatialEntity}
+import org.apache.spark.{SparkContext, TaskContext}
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.rdd.RDD
+
+import scala.collection.mutable.ArrayBuffer
+
+case class PartitionBlocking(var source: RDD[SpatialEntity], var target: RDD[SpatialEntity], thetaMsrSTR: String) extends  Blocking with Serializable {
+
+    var partitionsZones: Array[MBB] = Array()
+    def setPartitionsZones(boundaries: Array[MBB]): Unit = partitionsZones = boundaries
+
+    def adjustPartitionsZones() : Unit ={
+        val (thetaX, thetaY) = broadcastMap("theta").value.asInstanceOf[(Double, Double)]
+
+        partitionsZones = partitionsZones.map(mbb =>{
+            val maxX = math.ceil(mbb.maxX / thetaX).toInt
+            val minX = math.floor(mbb.minX / thetaX).toInt
+            val maxY = math.ceil(mbb.maxY / thetaY).toInt
+            val minY = math.floor(mbb.minY / thetaY).toInt
+
+            MBB(maxX, minX, maxY, minY)
+        })
+    }
+
+
+
+    /**
+     * index a spatial entities set. If acceptedBlocks is provided then the entities will be assigned
+     * only to blocks that exist in the accepted blocks set
+     *
+     * @param spatialEntitiesRDD the set to index
+     * @param acceptedBlocks the accepted blocks that the set can be indexed to
+     * @return an Array of block ids for each spatial entity
+     */
+    def index(spatialEntitiesRDD: RDD[SpatialEntity], acceptedBlocks: Set[(Int, Int)] = Set()): RDD[((Int, Int), ArrayBuffer[SpatialEntity])] ={
+        val acceptedBlocksBD = SparkContext.getOrCreate().broadcast(acceptedBlocks)
+        broadcastMap += ("acceptedBlocks" -> acceptedBlocksBD.asInstanceOf[Broadcast[Any]])
+        spatialEntitiesRDD.mapPartitions { seIter =>
+            val partitionZone = partitionsZones(TaskContext.getPartitionId())
+            val thetaMsr = broadcastMap("theta").value.asInstanceOf[(Double, Double)]
+            val acceptedBlocks = acceptedBlocksBD.value
+            val p = seIter.toArray.map(se => (indexSpatialEntity(se, acceptedBlocks, thetaMsr), se))
+                .map(b => (b._1.filter( coords => partitionZone.minX < coords._1 && partitionZone.maxX > coords._1 &&
+                                                  partitionZone.minY < coords._2 && partitionZone.maxY > coords._2 ), b._2))
+                .flatMap(b => b._1.map(id => (id, ArrayBuffer[SpatialEntity](b._2))))
+                .groupBy( _._1)
+                .map(p => (p._1, p._2.flatMap(ar => ar._2).to[ArrayBuffer]))
+            p.toIterator
+        }
+    }
+
+    /**
+     * Apply indexing
+     * @return RDD of blocks
+     */
+    override def apply(): RDD[Block] = {
+        initTheta(thetaMsrSTR)
+        adjustPartitionsZones()
+
+        val sourceIndex = index(source)
+        val sourceBlocks: Set[(Int, Int)] = sourceIndex.map(b => Set(b._1)).reduce(_++_)
+        val targetIndex = index(target, sourceBlocks)
+
+        val blocksIndex: RDD[((Int, Int), (ArrayBuffer[SpatialEntity], Option[ArrayBuffer[SpatialEntity]]))] = targetIndex.leftOuterJoin(sourceIndex)
+
+        // construct blocks from indexes
+        blocksIndex
+            .filter(_._2._2.isDefined)
+            .map { block =>
+                val blockCoords = block._1
+                val sourceIndex = block._2._2.get
+                val targetIndex = block._2._1
+                Block(blockCoords, sourceIndex, targetIndex)
+            }
+    }
+
+
+}
+
---
+++ src/main/scala/utils/Readers/SpatialReader.scala
@@ -0,0 +1,94 @@
+package utils.Readers
+
+import Blocking.PartitionBlocking
+import DataStructures.{MBB, SpatialEntity}
+import com.vividsolutions.jts.geom.Geometry
+import org.apache.spark.rdd.RDD
+import org.apache.spark.serializer.KryoSerializer
+import org.apache.spark.{SparkConf, SparkContext, TaskContext}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.datasyslab.geospark.enums.GridType
+import org.datasyslab.geospark.serde.GeoSparkKryoRegistrator
+import org.datasyslab.geospark.spatialPartitioning.SpatialPartitioner
+import org.datasyslab.geospark.spatialRDD.SpatialRDD
+import org.datasyslab.geosparksql.utils.{Adapter, GeoSparkSQLRegistrator}
+import org.apache.spark.sql.types.{LongType, StructField, StructType}
+import org.datasyslab.geospark.spatialPartitioning.quadtree.QuadRectangle
+
+
+object SpatialReader extends TReader {
+
+    var spatialPartitioner: SpatialPartitioner = _
+    var partitionsZones: Array[MBB] = Array()
+
+
+    def load(filepath: String, realIdField: String, geometryField: String, startIdFrom: Int = 0): RDD[SpatialEntity] ={
+        val extension = filepath.toString.split("\\.").last
+        extension match {
+            case "csv" =>
+                loadCSV(filepath, realIdField, geometryField, startIdFrom)
+
+            case _ =>
+                null
+        }
+
+    }
+
+    def loadCSV(filepath: String, realIdField: String, geometryField: String, startIdFrom: Int, delimiter: String = ",", header: Boolean = true): RDD[SpatialEntity] ={
+
+        val conf = new SparkConf()
+        conf.set("spark.serializer", classOf[KryoSerializer].getName)
+        conf.set("spark.kryo.registrator", classOf[GeoSparkKryoRegistrator].getName)
+        val sc = SparkContext.getOrCreate(conf)
+        val spark = SparkSession.getActiveSession.get
+
+        GeoSparkSQLRegistrator.registerAll(spark)
+
+        val inputDF = spark.read.format("csv")
+            .option("delimiter", delimiter)
+            .option("header", header)
+            .load(filepath)
+            .filter(r =>  ! r.getAs(geometryField).asInstanceOf[String].contains("EMPTY"))
+
+
+        val newSchema = StructType(inputDF.schema.fields ++ Array(StructField("ROW_ID", LongType, nullable = false)))
+
+        // Zip on RDD level
+        val rddWithId = inputDF.rdd.zipWithIndex
+        // Convert back to DataFrame
+        val dfZippedWithId =  spark.createDataFrame(rddWithId.map{ case (row, index) => Row.fromSeq(row.toSeq ++ Array(index))}, newSchema)
+
+
+        dfZippedWithId.createOrReplaceTempView("GEOMETRIES")
+
+        val geometryQuery: String =  """SELECT ST_GeomFromWKT(GEOMETRIES.""" + geometryField + """) AS WKT,  GEOMETRIES.""" + realIdField + """ AS REAL_ID, GEOMETRIES.ROW_ID AS ID FROM GEOMETRIES""".stripMargin
+        val spatialDF = spark.sql(geometryQuery)
+        val srdd = new SpatialRDD[Geometry]
+        srdd.rawSpatialRDD = Adapter.toRdd(spatialDF)
+
+        srdd.analyze()
+        if (spatialPartitioner == null) {
+            srdd.spatialPartitioning(GridType.QUADTREE)
+            spatialPartitioner = srdd.getPartitioner
+            val zones = srdd.partitionTree.getLeafZones
+            partitionsZones = zones.toArray(Array.ofDim[QuadRectangle](zones.size())).map(qr => MBB(qr.getEnvelope))
+            val p = srdd.spatialPartitionedRDD.rdd.mapPartitions({
+                iter =>
+                    Iterator(TaskContext.getPartitionId(), iter.toArray.map(_.toText).mkString("\n"))
+            }).collect()
+            print()
+        }
+        else
+            srdd.spatialPartitioning(spatialPartitioner)
+
+        srdd.spatialPartitionedRDD.rdd
+            .map{ g =>
+                    val ids = g.getUserData.asInstanceOf[String].split("\t")
+                    val realID = ids(0)
+                    val id = ids(1).toInt
+                    (g, realID, id)
+            }
+            .map{ case(g, realID, id) => SpatialEntity(id + startIdFrom, realID, g)}
+
+    }
+}
