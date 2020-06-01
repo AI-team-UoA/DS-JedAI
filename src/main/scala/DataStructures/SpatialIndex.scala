9c8580c4b21ac2e83cbd9b4f33a505b5dce11681
George <giwrgosmandi@gmail.com>
2020-06-01 12:05:35 +0300

    using arrays instead of list

--- src/main/scala/DataStructures/SpatialIndex.scala
+++ src/main/scala/DataStructures/SpatialIndex.scala
@@ -1,21 +1,21 @@
 package DataStructures
 
 import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
 class SpatialIndex(){
 
-    val index: mutable.HashMap[Int, mutable.HashMap[Int, mutable.ListBuffer[Int]]] = new mutable.HashMap()
+    val index: mutable.HashMap[Int, mutable.HashMap[Int, ArrayBuffer[Int]]] = new mutable.HashMap()
 
     def insert(c: (Int, Int), i: Int): Unit = {
         if (index.contains(c._1))
             if (index(c._1).contains(c._2))
                 index(c._1)(c._2) += i
             else
-                index(c._1).put(c._2, ListBuffer(i))
+                index(c._1).put(c._2, ArrayBuffer(i))
         else {
-            val l = ListBuffer[Int](i)
-            val h = mutable.HashMap[Int, ListBuffer[Int]]()
+            val l = ArrayBuffer[Int](i)
+            val h = mutable.HashMap[Int, ArrayBuffer[Int]]()
             h.put(c._2, l)
             index.put(c._1, h)
         }
@@ -23,6 +23,6 @@ class SpatialIndex(){
 
     def contains(c: (Int, Int)): Boolean = index.contains(c._1) && index(c._1).contains(c._2)
 
-    def get(c:(Int, Int)): ListBuffer[Int] = index(c._1)(c._2)
+    def get(c:(Int, Int)): ArrayBuffer[Int] = index(c._1)(c._2)
 
 }
