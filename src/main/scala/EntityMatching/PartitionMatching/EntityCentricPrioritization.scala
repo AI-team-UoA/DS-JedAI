29ad25e2569383cd8760e4adcb0d983d38e1536d
George <giwrgosmandi@gmail.com>
2020-07-31 15:03:31 +0300

    DE9IM

--- src/main/scala/EntityMatching/PartitionMatching/EntityCentricPrioritization.scala
+++ src/main/scala/EntityMatching/PartitionMatching/EntityCentricPrioritization.scala
@@ -8,7 +8,7 @@ import org.apache.spark.rdd.RDD
 import utils.Constants.Relation.Relation
 import utils.Constants.ThetaOption.ThetaOption
 import utils.Constants.WeightStrategy.WeightStrategy
-import utils.Utils
+import utils.{Constants, Utils}
 import utils.Readers.SpatialReader
 
 import scala.collection.mutable
@@ -72,7 +72,51 @@ case class EntityCentricPrioritization(joinedRDD: RDD[(Int, (Iterable[SpatialEnt
             .map(c => ((c._1.originalID, c._2.originalID), c._1.mbb.testMBB(c._2.mbb, relation) && c._1.relate(c._2, relation)))
     }
 
-    def getDE9IM: RDD[IM] = null
+    def getDE9IM: RDD[IM] = {
+        val k = budget / targetCount
+        joinedRDD
+        .filter(p => p._2._1.nonEmpty && p._2._2.nonEmpty)
+        .flatMap { p =>
+            val partitionId = p._1
+            val source: Array[SpatialEntity] = p._2._1.toArray
+            val target: Iterator[SpatialEntity] = p._2._2.toIterator
+            val sourceIndex = index(source, partitionId)
+            val sourceSize = source.length
+            val frequencies = new Array[Int](sourceSize)
+            val filteringFunction = (b: (Int, Int)) => sourceIndex.contains(b) && zoneCheck(partitionId)(b)
+            val pq = mutable.PriorityQueue[(Double, Int)]()(Ordering.by[(Double, Int), Double](_._1).reverse)
+
+            target
+                .map {e2 =>
+                    val sIndices:Array[((Int, Int), ArrayBuffer[Int])] = e2.index(thetaXY, filteringFunction).map(c => (c, sourceIndex.get(c)))
+                    util.Arrays.fill(frequencies, 0)
+                    sIndices.flatMap(_._2)foreach(i => frequencies(i) += 1)
+
+                    var wSum = 0d
+                    sIndices
+                        .flatMap{ case (c, indices) =>
+                            indices.filter(i => source(i).mbb.referencePointFiltering(e2.mbb, c, thetaXY) && source(i).mbb.testMBB(e2.mbb, Constants.Relation.INTERSECTS))}
+                        .foreach { i =>
+                            val e1 = source(i)
+                            val f = frequencies(i)
+                            val w = getWeight(f, e1, e2)
+                            wSum += w
+                            pq.enqueue((w, i))
+                        }
+                    val weight = if (pq.nonEmpty) wSum / pq.length else -1d
+                    val sz = if (k < pq.length) k.toInt else pq.length
+
+                    val weightedComparisons = for (_ <- 0 until sz) yield pq.dequeue()
+                    pq.clear()
+                    val topComparisons = weightedComparisons.map{case(_, i) => IM(source(i), e2)}
+                    (weight, topComparisons)
+                }
+                .filter(_._1 >= 0d)
+        }
+        // sort the comparisons based on their mean weight
+        .sortByKey(ascending = false)
+        .flatMap(_._2)
+    }
 
 }
 
