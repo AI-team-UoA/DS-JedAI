1232cd295840dc93c1a8de180e8ecd5391fcb536
George <giwrgosmandi@gmail.com>
2020-08-17 12:24:59 +0300

    space stats

--- src/main/scala/EntityMatching/SpaceStatsCounter.scala
+++ src/main/scala/EntityMatching/SpaceStatsCounter.scala
@@ -4,7 +4,6 @@ import DataStructures.{IM, MBB, SpatialEntity, SpatialIndex}
 import org.apache.log4j.{Level, LogManager}
 import org.apache.spark.TaskContext
 import org.apache.spark.rdd.RDD
-import org.apache.spark.storage.StorageLevel
 import utils.Constants.Relation
 import utils.Constants.ThetaOption.ThetaOption
 import utils.Readers.SpatialReader
@@ -19,48 +18,58 @@ case class SpaceStatsCounter(joinedRDD: RDD[(Int, (Iterable[SpatialEntity],  Ite
         val log = LogManager.getRootLogger
         log.setLevel(Level.INFO)
 
-        // todo adjust on theta
-        val mbbRDD = joinedRDD.flatMap(p => p._2._1.map(se => se.mbb) ++ p._2._2.map(se => se.mbb)).persist(StorageLevel.MEMORY_AND_DISK)
-        val mbbMinX = math.floor(mbbRDD.map(mbb => mbb.minX).min())
-        val mbbMinY = math.floor(mbbRDD.map(mbb => mbb.minY).min())
-        val mbbMaxX = math.ceil(mbbRDD.map(mbb => mbb.maxX).max())
-        val mbbMaxY = math.ceil(mbbRDD.map(mbb => mbb.maxY).max())
-        val totalTiles = (mbbMaxX - mbbMinX)*(mbbMaxY - mbbMinY)
-        log.info("Total Tiles: " + totalTiles)
-        mbbRDD.unpersist()
+        val source: RDD[SpatialEntity] = joinedRDD.flatMap(_._2._1.map(se => (se.originalID, se))).distinct().map(_._2).setName("Source").cache()
+        val target: RDD[SpatialEntity] = joinedRDD.flatMap(_._2._2.map(se => (se.originalID, se))).distinct().map(_._2).setName("target").cache()
 
-        val allComparisonsRDD: RDD[((Int, Int), (SpatialEntity, SpatialEntity))] = joinedRDD
+        log.info("Source count: " + source.count())
+        log.info("Target count: " + target.count())
+
+        val sourceTiles:  RDD[(Int, Int)] = source.flatMap(se => se.index(thetaXY)).setName("SourceTiles").cache()
+        val targetTiles: RDD[(Int, Int)] = target.flatMap(se => se.index(thetaXY)).setName("TargetTiles").cache()
+
+        val tiles = sourceTiles.union(targetTiles).distinct().count()
+        log.info("All Tiles: " + tiles)
+
+        val ssePerTile: RDD[((Int, Int), Int)] = sourceTiles.map((_,1)).reduceByKey(_ + _)
+        val tsePerTile: RDD[((Int, Int), Int)] = sourceTiles.map((_,1)).reduceByKey(_ + _)
+
+        val pairTiles = ssePerTile.join(tsePerTile).map( p => p._2._1*p._2._2).sum()
+        log.info("Pairs Tiles: " + pairTiles)
+
+        source.unpersist()
+        target.unpersist()
+        sourceTiles.unpersist()
+        targetTiles.unpersist()
+
+        val uniqueTilesRDD: RDD[((Int, Int), (SpatialEntity, SpatialEntity))] = joinedRDD
             .filter(p => p._2._1.nonEmpty && p._2._2.nonEmpty)
             .flatMap { p =>
                 val source: Array[SpatialEntity] = p._2._1.toArray
                 val target: Iterator[SpatialEntity] = p._2._2.toIterator
                 val sourceIndex = index(source)
                 val filteringFunction = (b: (Int, Int)) => sourceIndex.contains(b)
+                val pid = p._1
+                val partition = partitionsZones(pid)
 
                 target.flatMap { targetSE =>
                     targetSE
                         .index(thetaXY, filteringFunction)
                         .flatMap(c => sourceIndex.get(c).map(j => (c, source(j))))
-                        .map(p => (p._2.originalID+p._1._1 +"-"+ targetSE.originalID+p._1._2, (p._1, (p._2, targetSE))))
+                        .filter{case(c, se) => se.referencePointFiltering(targetSE, c, thetaXY, Some(partition))}
+                        .map {case(c, se) => (c, (se, targetSE))}
                 }
-            }
-            .reduceByKey((_, b) => b)
-            .map(_._2)
-            .persist(StorageLevel.MEMORY_AND_DISK)
+            }.setName("UniqueTilesRDD").cache()
 
-        val pairsTiles = allComparisonsRDD.count()
-        val uniquePairs = allComparisonsRDD.filter{case (c, (sSE, tSE)) => sSE.referencePointFiltering(tSE, c, thetaXY, Some(partitionsZones(TaskContext.getPartitionId())))}.count()
-        val intersectingPairs = allComparisonsRDD.filter{case (_, (sSE, tSE)) => sSE.testMBB(tSE, Relation.INTERSECTS, Relation.TOUCHES)}.count()
-        val truePairs = allComparisonsRDD
-            .filter{case (c, (sSE, tSE)) => sSE.referencePointFiltering(tSE, c, thetaXY, Some(partitionsZones(TaskContext.getPartitionId()))) && sSE.testMBB(tSE, Relation.INTERSECTS, Relation.TOUCHES)}
-            .filter{case (_, (sSE, tSE)) => IM(sSE, tSE).relate}
-            .map { case (c, (sSE, tSE)) => (c, sSE.originalID, tSE.originalID)}
-            .distinct()
-            .count()
-        log.info("Pairs Tiles: " + pairsTiles)
-        log.info("Unique Pairs: " + uniquePairs)
-        log.info("Intersecting Pairs: " + intersectingPairs)
-        log.info("True Pairs: " + truePairs)
+        val intersectingTiles = uniqueTilesRDD.filter{ case (c, (sSE, tSE)) => sSE.testMBB(tSE, Relation.INTERSECTS, Relation.TOUCHES)}
+        val truePairs = uniqueTilesRDD.filter{ case (c, (sSE, tSE)) => sSE.testMBB(tSE, Relation.INTERSECTS, Relation.TOUCHES)}.filter{case (_, (sSE, tSE)) => IM(sSE, tSE).relate}
+
+        log.info("Unique Tiles: " + uniqueTilesRDD.count())
+        log.info("Intersecting Pairs: " + intersectingTiles.count())
+        log.info("True Pairs: " + truePairs.count())
+        log.info("")
+
+        uniqueTilesRDD.unpersist()
+
     }
 
     /**
