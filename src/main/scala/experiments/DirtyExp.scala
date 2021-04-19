db5516dc2607ca1e456631af9160494d7b6189d1
George <giwrgosmandi@gmail.com>
2021-04-19 14:08:26 +0300

    single set execution

---
+++ src/main/scala/experiments/DirtyExp.scala
@@ -0,0 +1,111 @@
+package experiments
+
+import java.util.Calendar
+
+import interlinkers.DirtyGIAnt
+import model.Entity
+import org.apache.log4j.{Level, LogManager, Logger}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.serializer.KryoSerializer
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.{SparkConf, SparkContext}
+import org.datasyslab.geospark.serde.GeoSparkKryoRegistrator
+import utils.Constants.GridType
+import utils.readers.Reader
+import utils.{ConfigurationParser, Utils}
+
+object DirtyExp {
+
+    def main(args: Array[String]): Unit = {
+        Logger.getLogger("org").setLevel(Level.ERROR)
+        Logger.getLogger("akka").setLevel(Level.ERROR)
+        val log = LogManager.getRootLogger
+        log.setLevel(Level.INFO)
+
+        val sparkConf = new SparkConf()
+            .setAppName("DS-JedAI")
+            .set("spark.serializer", classOf[KryoSerializer].getName)
+            .set("spark.kryo.registrator", classOf[GeoSparkKryoRegistrator].getName)
+
+        val sc = new SparkContext(sparkConf)
+        val spark: SparkSession = SparkSession.builder().getOrCreate()
+
+        // Parsing input arguments
+        @scala.annotation.tailrec
+        def nextOption(map: OptionMap, list: List[String]): OptionMap = {
+            list match {
+                case Nil => map
+                case ("-c" | "-conf") :: value :: tail =>
+                    nextOption(map ++ Map("conf" -> value), tail)
+                case ("-p" | "-partitions") :: value :: tail =>
+                    nextOption(map ++ Map("partitions" -> value), tail)
+                case "-gt" :: value :: tail =>
+                    nextOption(map ++ Map("gt" -> value), tail)
+                case "-print" :: tail =>
+                    nextOption(map ++ Map("print" -> "true"), tail)
+                case "-o" :: value :: tail =>
+                    nextOption(map ++ Map("output" -> value), tail)
+                case _ :: tail =>
+                    log.warn("DS-JEDAI: Unrecognized argument")
+                    nextOption(map, tail)
+            }
+        }
+
+        val argList = args.toList
+        type OptionMap = Map[String, String]
+        val options = nextOption(Map(), argList)
+
+        if (!options.contains("conf")) {
+            log.error("DS-JEDAI: No configuration file!")
+            System.exit(1)
+        }
+
+        val confPath = options("conf")
+        val conf = ConfigurationParser.parseDirty(confPath)
+        val partitions: Int = if (options.contains("partitions")) options("partitions").toInt else conf.getPartitions
+        val gridType: GridType.GridType = if (options.contains("gt")) GridType.withName(options("gt").toString) else conf.getGridType
+        val output: String = if (options.contains("output")) options("output") else conf.getOutputPath
+        val print = options.getOrElse("print", "false").toBoolean
+
+        val startTime = Calendar.getInstance().getTimeInMillis
+
+        // reading source dataset
+        val reader = Reader(partitions, gridType)
+        val sourceRDD: RDD[(Int, Entity)] = reader.loadSource(conf.source)
+        sourceRDD.persist(StorageLevel.MEMORY_AND_DISK)
+
+        Utils(sourceRDD.map(_._2.mbr), conf.getTheta, reader.partitionsZones)
+        log.info(s"DS-JEDAI: Source was loaded into ${sourceRDD.getNumPartitions} partitions")
+
+        val giant = DirtyGIAnt(sourceRDD.map(_._2), Utils.getTheta)
+        val imRDD = giant.getDE9IM
+
+        if (print) {
+            // imRDD.persist(StorageLevel.MEMORY_AND_DISK)
+            val (totalContains, totalCoveredBy, totalCovers, totalCrosses, totalEquals, totalIntersects,
+            totalOverlaps, totalTouches, totalWithin, verifications, qp) = Utils.countAllRelations(imRDD)
+
+            val totalRelations = totalContains + totalCoveredBy + totalCovers + totalCrosses + totalEquals +
+                totalIntersects + totalOverlaps + totalTouches + totalWithin
+            log.info("DS-JEDAI: Total Verifications: " + verifications)
+            log.info("DS-JEDAI: Qualifying Pairs : " + qp)
+
+            log.info("DS-JEDAI: CONTAINS: " + totalContains)
+            log.info("DS-JEDAI: COVERED BY: " + totalCoveredBy)
+            log.info("DS-JEDAI: COVERS: " + totalCovers)
+            log.info("DS-JEDAI: CROSSES: " + totalCrosses)
+            log.info("DS-JEDAI: EQUALS: " + totalEquals)
+            log.info("DS-JEDAI: INTERSECTS: " + totalIntersects)
+            log.info("DS-JEDAI: OVERLAPS: " + totalOverlaps)
+            log.info("DS-JEDAI: TOUCHES: " + totalTouches)
+            log.info("DS-JEDAI: WITHIN: " + totalWithin)
+            log.info("DS-JEDAI: Total Discovered Relations: " + totalRelations)
+        }
+        else
+            Utils.exportRDF(imRDD, output)
+        val endTime = Calendar.getInstance().getTimeInMillis
+        log.info("DS-JEDAI: Total Execution Time: " + (endTime - startTime) / 1000.0)
+
+    }
+}
---
+++ src/main/scala/interlinkers/DirtyGIAnt.scala
@@ -0,0 +1,64 @@
+package interlinkers
+
+import model.{Entity, IM, MBR, SpatialIndex}
+import org.apache.spark.{SparkContext, TaskContext}
+import org.apache.spark.rdd.RDD
+import utils.Constants.Relation
+import utils.Constants.Relation.Relation
+import utils.Utils
+
+case class DirtyGIAnt(source:RDD[Entity], thetaXY: (Double, Double)) {
+
+    val partitionsZones: Array[MBR] = SparkContext.getOrCreate().broadcast(Utils.getZones).value
+
+    /**
+     * Extract the candidate geometries from source, using spatial index
+     * @param e2 target geometry
+     * @param source array of source geometries
+     * @param index spatial index
+     * @param partitionZone examining partition
+     * @param relation examining relations
+     * @return a sequence of candidate geometries
+     */
+    def getCandidates(e2: Entity, source: Array[Entity], index: SpatialIndex, partitionZone: MBR, relation: Relation): Seq[Entity] =
+        e2.index(thetaXY, index.contains).view
+            .flatMap(block => index.get(block).map(i => (block, i)))
+            .filter{ case (block, i) => source(i).filter(e2, relation, block, thetaXY, Some(partitionZone))}
+            .map{case (_, i) => source(i) }
+            .force
+
+    /**
+     * index a list of spatial entities
+     *
+     * @param entities list of spatial entities
+     * @return a SpatialIndex
+     */
+    def index(entities: Array[Entity]): SpatialIndex = {
+        val spatialIndex = new SpatialIndex()
+        entities.zipWithIndex.foreach { case (se, i) =>
+            val indices: Seq[(Int, Int)] = se.index(thetaXY)
+            indices.foreach(c => spatialIndex.insert(c, i))
+        }
+        spatialIndex
+    }
+
+
+
+    def getDE9IM: RDD[IM] = {
+        source
+            .mapPartitions { p =>
+                val partition = partitionsZones(TaskContext.getPartitionId())
+                val source: Array[Entity] = p.toArray
+                val sourceIndex = index(source)
+                sourceIndex.getIndices.flatMap { b: (Int, Int) =>
+                    val candidates = sourceIndex.get(b)
+                    for (i <- candidates;
+                         j <- candidates
+                         if i != j
+                         if source(i).filter(source(j), Relation.DE9IM, b, thetaXY, Some(partition)))
+                        yield IM( source(i), source(j))
+                }.toIterator
+            }
+    }
+
+}
